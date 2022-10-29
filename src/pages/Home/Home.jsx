2be483378ce26d7c745962722871377d2850bde1
Maratoos <kudushev01@mail.ru>
2022-10-29 10:40:05 +0600

    ChatUsersCreated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -27,13 +27,6 @@ export const Home = () => {
             </div>
             <div className='chat__bottom'>
               <input placeholder="Написать сообщение" type="text" />
-              <div className='input__images'>
-                <AddCircleIcon />
-                <RedeemIcon />
-                <GifIcon />
-                <InsertDriveFileIcon />
-                <AddReactionIcon />
-              </div>
             </div>
           </div>
           <ChatUsers />
